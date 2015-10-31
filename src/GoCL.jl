e13de643ae0b3289edf9b121439862208367d15a
andrew cooke <andrew@acooke.org>
2015-10-31 18:34:49 -0300

    improve docs

--- src/GoCL.jl
+++ src/GoCL.jl
@@ -2,17 +2,21 @@
 module GoCL
 
 export @forall, @forall_fold,
-       Point, black, empty, white, 
+       Point, black, empty, white, other,
        Board, point, move, move!,
        Position
-
 
 using AutoHashEquals
 import Base: ==, print
 
 
-# execute the same code at eahc point on the board (tries to guarantee that
-# the julia code is structured similarly to an opencl kernel).
+
+# --- macros
+
+
+"""execute the same code at each point on the board (tries to
+guarantee that the julia code is structured similarly to an opencl
+kernel)."""
 macro forall(i, j, block)
     :(for $(esc(i)) in 1:19
           for $(esc(j)) in 1:19
@@ -21,8 +25,8 @@ macro forall(i, j, block)
       end)
 end
 
-# as forall, but also include a final reduction (again, easy to
-# implement in a kernel).
+"""as forall, but also include a final reduction (again, easy to
+implement in a kernel)."""
 macro forall_fold(i, j, f, z, r, block)
     :(for $(esc(i)) in 1:19
           for $(esc(j)) in 1:19
@@ -32,8 +36,8 @@ macro forall_fold(i, j, f, z, r, block)
       foldl($(esc(f)), $(esc(z)), $(esc(r))))
 end
 
-# iterate over neigbours - explicit loop (not parallelized) (to reduce
-# board lookup dx=0 should be grouped)
+"""iterate over neigbours - explicit loop (not parallelized) (to
+reduce board lookup dx=0 should be grouped)."""
 macro forneighbours(x, y, xx, yy, block)
     :(for (dx, dy) in ((0, 1), (0, -1), (1, 0), (-1, 0))
           $(esc(xx)), $(esc(yy)) = $(esc(x)) + dx, $(esc(y)) + dy      
@@ -44,37 +48,70 @@ macro forneighbours(x, y, xx, yy, block)
 end
 
 
+# --- data structures
+
+
+"""a point on the board - empty, or a stone."""
 @enum Point empty=0 black=1 white=-1
+
+"""if the point is a stone, give a stone of the other colour.  if
+empty, return empty."""
 other(t::Point) = t == empty ? empty : (t == black ? white : black)
 
-# 19 trits just fit into 32 bits
+
+"""a row of 19 trits fits nicely in 32 bits binary."""
 typealias Row UInt32
+
+"""a row of 19 zeros (signed trits)."""
 emptyrow = Row(sum([3^(n-1) for n in 1:19]))
 
+"""a trit-map of the entire board."""
 @auto_hash_equals immutable Board
     rows::Vector{Row}
     Board() = new(fill(emptyrow, 19))
     Board(b::Board) = new(copy(b.rows))
 end
 
+"""extract the point at a given (x, y)."""
 point(r::Row, x) = Point(mod(div(r, 3^(x-1)), 3)-1)
+
+"""extract the point at a given (x, y)."""
 point(b::Board, x, y) = point(b.rows[y], x)
 
+
+"""identify all points common to a single group.
+
+this is an optimisation - a cached, updated summary, that makes other
+operations (updating the board on a move) more efficient.  it may not
+be necessary."""
 @auto_hash_equals immutable Groups
     index::Array{UInt8, 2}
     Groups() = new(zeros(UInt8, 19, 19))
     Groups(g::Groups) = new(copy(g.index))
 end
 
+
+"""a map of cartesian signed distances to the nearest non-empty point,
+where the sign indicates whether the nearest point is black or white
+(via the Point enumeration).
+
+distances are offset by 1, so a value of +/-1 indicates that the point
+contains a stone.  a value of zero is used initially (replaced on
+first move by flood_to_point!) and as a marker for deleted groups
+(replaced by flood_dead_group!).
+
+this contains almost all the information about the state of play (the
++/-1 values duplicate Board).  it may be used as input to a neural
+net."""
 @auto_hash_equals immutable Flood
-    # distances have an offset of 1 (so occupied points have a
-    # distance of 1), with black positive and white negative.  a value
-    # of zero is used to indicate that a group was removed.
     distances::Array{Int8, 2}
     Flood() = new(zeros(Int8, 19, 19))
     Flood(f::Flood) = new(copy(f.distances))
 end
 
+
+"""a single position (implicitly, in a search tree).  combines Board,
+Groups and Flood."""
 @auto_hash_equals immutable Position
     board::Board
     groups::Groups
@@ -83,6 +120,9 @@ end
     Position(p::Position) =
         new(Board(p.board), Groups(p.groups), Flood(p.flood))
 end
+
+
+# --- display
 
 
 const markers = Set([4, 10, 16])
@@ -137,6 +177,9 @@ function print(io::IO, p::Position)
     print(io, "\n")
     print(io, p.flood)
 end
+
+
+# --- update state on move
 
 
 function lowest_empty_group(g::Groups)
